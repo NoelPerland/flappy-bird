4f691970cb047562c12f12b7eec7744530e52b14
Noel Perland <noel.perland@chasacademy.se>
2025-06-14 17:31:38 +0200

    Bird is flying! Not flapping yet but can tap.

--- App.js
+++ App.js
@@ -10,8 +10,16 @@ import {
   Easing,
   withSequence,
   withRepeat,
+  useFrameCallback,
 } from "react-native-reanimated";
 import { useEffect } from "react";
+import {
+  GestureHandlerRootView,
+  GestureDetector,
+  Gesture,
+} from "react-native-gesture-handler";
+
+const GRAVITY = 600; // pixels per frame
 
 const App = () => {
   const { width, height } = useWindowDimensions();
@@ -24,6 +32,17 @@ const App = () => {
 
   const x = useSharedValue(width);
 
+  const birdY = useSharedValue(0);
+  const birdYVelocity = useSharedValue(100);
+
+  useFrameCallback(({ timeSincePreviousFrame: dt }) => {
+    if (!dt) {
+      return;
+    }
+    birdY.value = birdY.value + (birdYVelocity.value * dt) / 1000;
+    birdYVelocity.value = birdYVelocity.value + (GRAVITY * dt) / 1000;
+  });
+
   useEffect(() => {
     x.value = withRepeat(
       withSequence(
@@ -34,48 +53,54 @@ const App = () => {
     );
   }, []);
 
+  const gesture = Gesture.Tap().onStart(() => {
+    birdYVelocity.value = -300;
+  });
+
   const pipeOffest = 0;
 
   return (
-    <Canvas style={{ width, height }}>
-      {/*BG */}
-      <SkiaImage image={bg} width={width} height={height} fit={"cover"} />
+    <GestureHandlerRootView>
+      <GestureDetector gesture={gesture}>
+        <View style={{ flex: 1 }}>
+          <Canvas style={{ width, height }}>
+            {/* BG */}
+            <SkiaImage image={bg} width={width} height={height} fit={"cover"} />
 
-      {/*Pipe top*/}
-      <SkiaImage
-        image={pipeTop}
-        y={pipeOffest - 300}
-        x={x}
-        width={100}
-        height={600}
-      />
-      {/*Pipe bottom*/}
-      <SkiaImage
-        image={pipeBottom}
-        y={height - 300 + pipeOffest}
-        x={x}
-        width={100}
-        height={600}
-      />
-      {/* Base */}
-      <SkiaImage
-        image={base}
-        width={width}
-        height={150}
-        x={0}
-        y={height - 90}
-        fit={"cover"}
-      />
+            {/* Pipe Top */}
+            <SkiaImage
+              image={pipeTop}
+              y={pipeOffest - 300}
+              x={x}
+              width={100}
+              height={600}
+            />
 
-      {/*Brid */}
-      <SkiaImage
-        image={bird}
-        y={height / 2 - 18}
-        x={100}
-        width={48}
-        height={36}
-      />
-    </Canvas>
+            {/* Pipe Bottom */}
+            <SkiaImage
+              image={pipeBottom}
+              y={height - 300 + pipeOffest}
+              x={x}
+              width={100}
+              height={600}
+            />
+
+            {/* Base */}
+            <SkiaImage
+              image={base}
+              width={width}
+              height={150}
+              x={0}
+              y={height - 90}
+              fit={"cover"}
+            />
+
+            {/* Bird */}
+            <SkiaImage image={bird} y={birdY} x={100} width={48} height={36} />
+          </Canvas>
+        </View>
+      </GestureDetector>
+    </GestureHandlerRootView>
   );
 };
 
